55dc76434d07f405c44ef1ab4b7bcb54d2e6d024
Zoe Mendez <zoemendez316@gmail.com>
2022-07-19 09:15:57 -0600

    commit tree 7e7ffd006a61d6c39f3cff9744400316da3e9293

--- index.js
+++ index.js
@@ -21,8 +21,11 @@ function deleteFromEmployeeByKey(employee, key) {
     const newEmployee = { ...employee };
     delete newEmployee[key];
     return newEmployee
+
 }
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
     delete employee[key]
+
     return employee
 }
